527c6804820a0c37af39550a099d55fb184049c3
Slater Black <sjblack139@gmail.com>
2023-11-29 17:18:03 +1000

    alert created

--- reactApp1/src/App.tsx
+++ reactApp1/src/App.tsx
@@ -1,7 +1,31 @@
-import ListGroup from './components/ListGroup';
+/*  Content to add a list to a page
 
-function App(){
-  return <div><ListGroup /></div>;
+  import ListGroup from "./components/ListGroup";
+
+  let items = ["New York", "Alaska", "South Africa", "Greece", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
+*/
+
+import Alert from "./components/alert";
+
+function App() {
+  return (
+    <div>
+      <Alert>Hello World</Alert>
+    </div>
+  );
 }
 
 export default App;
