4536c41c227669ae3bd1bd7e7fe1372af3593836
Slater Black <sjblack139@gmail.com>
2023-11-29 17:57:36 +1000

    button function added

--- reactApp1/src/App.tsx
+++ reactApp1/src/App.tsx
@@ -1,29 +1,25 @@
-/*  Content to add a list to a page
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/alert";
+import Button from "./components/Button";
 
-  import ListGroup from "./components/ListGroup";
-
+function App() {
   let items = ["New York", "Alaska", "South Africa", "Greece", "Paris"];
 
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
+
   return (
     <div>
+      <Alert>Hello World</Alert>
+      <Button colour="secondary" onClick={() => console.log("Clicked")}>
+        My Button
+      </Button>
       <ListGroup
         items={items}
         heading="Cities"
         onSelectItem={handleSelectItem}
       />
-    </div>
-  );
-*/
-
-import Alert from "./components/alert";
-
-function App() {
-  return (
-    <div>
-      <Alert>Hello World</Alert>
     </div>
   );
 }
