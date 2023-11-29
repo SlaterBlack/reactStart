cef814c1cca74ef4bb920ab7626981769462bb40
Slater Black <sjblack139@gmail.com>
2023-11-29 18:34:02 +1000

    button can be deleted

--- reactApp1/src/App.tsx
+++ reactApp1/src/App.tsx
@@ -1,9 +1,11 @@
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/alert";
 import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
   let items = ["New York", "Alaska", "South Africa", "Greece", "Paris"];
+  const [alertVisible, setAlertVisibility] = useState(false);
 
   const handleSelectItem = (item: string) => {
     console.log(item);
@@ -11,8 +13,10 @@ function App() {
 
   return (
     <div>
-      <Alert>Hello World</Alert>
-      <Button colour="secondary" onClick={() => console.log("Clicked")}>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>Hello World</Alert>
+      )}
+      <Button colour="secondary" onClick={() => setAlertVisibility(true)}>
         My Button
       </Button>
       <ListGroup
